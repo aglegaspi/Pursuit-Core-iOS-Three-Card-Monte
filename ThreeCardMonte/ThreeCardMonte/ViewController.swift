ab80e0a2a87767684958fb34d2fba7883a343569
aglegaspi <lex83nyc@gmail.com>
2019-07-29 18:08:01 -0400

    starting over

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -9,31 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
 
-    @IBOutlet weak var cardOne: UIButton!
-    let kingImage = UIImage(named: "kingCard")
-    
-    @IBAction func flipCardOne(_ sender: UIButton) {
-        cardOne.setBackgroundImage(kingImage, for: UIControl.State.normal)
+    @IBOutlet weak var cardOne, cardTwo, cardThree: UIButton!
+
+    @IBAction func flipCard(_ sender: UIButton) {
+        cardOne.setBackgroundImage(UIImage(randomImg), for: UIControl.State.normal)
     }
     
     
     // flip one image
-    
     // flip all three. hint: array
-    
     // then make randomizer
     
     override func viewDidLoad() {
     super.viewDidLoad()
+        
         //this is where the randomizer will go
         // it will pick the winning card. each button respond in that way.
-        
         // 3 buttons if you click winning that's the winner else you lost.
-        
-    // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
   }
 
 
